415a6424d6e7d32ad23fd0de0d77aa9a5b20ce60
nicomilsz <nico@MacBook-Air-de-Fernanda.local>
2023-08-23 20:44:49 -0300

    app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, '../public')))
 
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT,()=>console.log(`Servidor corriendo en el puerto ${port}`))
+app.listen(PORT,()=>console.log(`Servidor corriendo en el puerto ${PORT}`))
 
 
 app.get("/",(req,res)=>{
